480bf9367c49bbfbbc7989b8ddf55cf7ec007bdc
gun-park7 <10171@krmpublicschool.edu.in>
2026-06-15 18:41:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function showMessage() {
+function claimReward() {
 
-    document.getElementById("hiddenMessage").innerHTML =
-    "I hope one bad evening doesn't become a bad memory. 😭";
+    document.getElementById("reward").innerHTML = `
+
+    🏆 QUEST COMPLETED
+
+    <br><br>
+
+    +100 Respect
+
+    <br>
+
+    +50 Patience
+
+    <br>
+
+    +1 Chance To See What You Wanted To Show Me 😭
+
+    <br><br>
+
+    Friendship Status:
+    Restoring... 🌷
+
+    `;
 
 }
